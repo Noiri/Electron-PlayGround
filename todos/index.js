66d345c287a08dab8d4dfac6aee7a7d7f05d8416
Noiri <suleipenn@gmail.com>
2019-03-28 01:20:28 +0900

    implemented add new todo form and quit button

---
+++ todos/index.js
@@ -0,0 +1,52 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu } = electron;
+
+//スコープの問題に対処するためにここに宣言するのが一般的らしい
+let mainWindow;
+let addWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow(){
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { 
+                label: 'New Todo' ,
+                click(){ createAddWindow(); }
+            },
+            {
+                label:  'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q': 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+
+
+if(process.platform === 'darwin'){
+    menuTemplate.unshift({
+        label: app.getName()
+    });
+}
